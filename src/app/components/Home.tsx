a79e25e4237c740b983ac5a897b6d492770554a1
AreebaSadiq23 <areebasadiq41@gmail.com>
2024-11-01 19:30:41 +0500

    update home file

--- src/app/components/Home.tsx
+++ src/app/components/Home.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import styles from '../styles/HomeSection.module.css';
+import Link from 'next/link';
+
 
 const HomeSection: React.FC = () => {
     return (
@@ -7,7 +9,9 @@ const HomeSection: React.FC = () => {
             <div className={styles.content}>
                 <h1 className={styles.name}>Hi 👋I am Areeba</h1>  
                 <h2 className={styles.title}>Frontend Developer</h2> 
-                <button className={styles.button}>Hire Me</button>
+                <Link href="#contact">
+        <button className={styles.button}>Hire Me</button>
+      </Link>
             </div>
         </section>
     );
