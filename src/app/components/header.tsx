09604e4191f559032e2bc891482bec93e30bc3b6
AreebaSadiq23 <areebasadiq41@gmail.com>
2024-11-01 18:57:25 +0500

    update contact and header file

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,12 +1,28 @@
+'use client';
+import { useState } from 'react';
 import styles from '../styles/Header.module.css';
 import Link from 'next/link';
+import { FaBars, FaTimes } from 'react-icons/fa';
 
 const Header: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <header className={styles.header}>
       <div className={styles.headerContainer}>
-        <div className={styles.logo}>Areeba....</div>
-        <nav>
+        <div className={styles.logo}>Areeba...</div>
+        
+        {/* Toggle button */}
+        <button className={styles.hamburger} onClick={toggleMenu}>
+          {isOpen ? <FaTimes /> : <FaBars />}
+        </button>
+        
+        {/* Navigation menu */}
+        <nav className={`${styles.nav} ${isOpen ? styles.active : ''}`}>
           <Link href="#home">Home</Link>
           <Link href="#about">About</Link>
           <Link href="#skills">Skills</Link>
@@ -17,4 +33,4 @@ const Header: React.FC = () => {
   );
 };
 
-export default Header;       
+export default Header;
