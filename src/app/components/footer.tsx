61d4578d15b8c6bbfabc71381e47cd7a7c4d4057
Ravish Fayyaz <160616153+ravishfayyaz555@users.noreply.github.com>
2024-12-20 19:25:33 +0500

    Update footer.tsx

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -3,7 +3,7 @@ import styles from '../styles/Footer.module.css'
 const Footer: React.FC = () => {
     return (
         <footer className={styles.footer}>
-        <p>&copy; {new Date().getFullYear()} Areeba Sadiq. All Rights Reserved.</p>
+        <p>&copy; {new Date().getFullYear()} Ravish Fayyaz. All Rights Reserved.</p>
         </footer>
     );
 };
